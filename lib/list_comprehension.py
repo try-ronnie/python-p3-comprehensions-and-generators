6549bd71fe27f14ff77f84684186b35fa40ad30f
ronniegikandi <kiiruronnie42@gmil.com>
2025-12-05 14:36:59 +0300

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+     even_list = [num for num in num_list if num % 2 == 0]
+     return even_list
+     pass
 
 def make_exclamation(sentence_list):
-    pass
+     exclamation_list = [sentence + "!" for sentence in sentence_list]
+     return exclamation_list
+     pass
